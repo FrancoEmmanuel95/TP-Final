1d12285f856f083d1471bf0825b986db48f17941
santiagon1993 <82768531+santiagon1993@users.noreply.github.com>
2022-06-08 18:39:32 -0300

    Add files via upload

--- consumos_header.h
+++ consumos_header.h
@@ -14,6 +14,19 @@ typedef struct
     int baja; /// 0 si est� activo - 1 si est� eliminado
 }stConsumos;
 
+typedef struct
+{
+    int id; /// campo �nico y autoincremental
+    int nroCliente;
+    char nombre[30];
+    char apellido[30];
+    char dni[10];
+    char email[30];
+    char domicilio[45];
+    char movil[12];
+    int eliminado; /// 0 si est� activo - 1 si est� eliminado
+} stCliente;
+
 stConsumos altaConsumos(char archivo [],char archivoCliente, stConsumos consumo, stCliente clientes);
 int validacionConsumo (char archivo[], stConsumos a);
 int validacionDiaMesAnio (stConsumos consumo);
